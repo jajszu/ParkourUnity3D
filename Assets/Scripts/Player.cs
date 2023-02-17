8e1d82c157044bb82980d92ed25a6ed8886a6ef4
jajszu <bartekjaje@gmail.com>
2023-02-17 21:59:27 +0100

    halo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    private Vector2 input;
     // Start is called before the first frame update
 
+    private CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    public float speed = 12f;
+    public float gravity = -20f;
+    public float jumpHeigh = 3f;
+    public float groundDistance = 0.4f;
+
+    bool isGrounded;
+    Vector3 velocity;
     private void Awake()
     {
-        
+        controller= GetComponent<CharacterController>();
     }
 
     void Start()
@@ -21,7 +30,28 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        input.x = Input.GetAxisRaw("Horizontal");
-        input.y = Input.GetAxisRaw("Vertical");
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded&&velocity.y< 0)
+        {
+            velocity.y = -2f;
+        }
+
+
+       float inputX = Input.GetAxisRaw("Horizontal");
+       float inputY = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = transform.right * inputX + transform.forward * inputY;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump")&&isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeigh * -2f * gravity);
+        }
+
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
