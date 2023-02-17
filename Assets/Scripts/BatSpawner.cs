9116407949d2b3ef2d8f7d88db0ae4555c0f2308
Patryk Czekaj <patryk13290@gmail.com>
2023-02-17 21:54:53 +0100

    skrypty do nietoperza

---
+++ Assets/Scripts/BatSpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> spawners;
+    [SerializeField]
+    private GameObject batPrefab;
+
+    public float minSpawnTime = 1.0f;
+    public float maxSpawnTime = 3.0f;
+
+    private bool spawning = true;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (spawning)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+            Instantiate(batPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void StartSpawning()
+    {
+        spawning = true;
+    }
+
+    public void StopSpawning()
+    {
+        spawning = false;
+    }
+}
