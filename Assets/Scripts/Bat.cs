713313d5b0a7ad7977d1a98e3e779d17ffdcabc9
Patryk Czekaj <patryk13290@gmail.com>
2023-02-21 20:24:11 +0100

    Dziedziczenie

--- Assets/Scripts/Bat.cs
+++ Assets/Scripts/Bat.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bat : Fighter
+public class Bat : Enemy
 {
     // Start is called before the first frame update
     void Start()
