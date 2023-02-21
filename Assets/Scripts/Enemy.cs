26900d0db168bc195fed6c5139d1c92c3e4322d5
Patryk Czekaj <patryk13290@gmail.com>
2023-02-21 21:17:59 +0100

    nic specjalnego

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Fighter
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    protected int damage = 10;
 }
